42f413f9df71d0d783f56e08d852aecdc28150ac
David Foldesi <david.foldesi@doclerholding.com>
2021-09-01 12:24:10 +0200

    Implementing the LoginComponent's class

--- src/component/login/LoginComponent.tsx
+++ src/component/login/LoginComponent.tsx
@@ -1,24 +1,63 @@
 import React from "react";
-import { Container, Row, Col, InputGroup, FormLabel, FormControl, Card} from "react-bootstrap";
-
+import { Container, Row, Col, InputGroup, FormLabel, FormControl, Card, FormGroup, Button, Form, Modal} from "react-bootstrap";
 import VerticalAlignComponent from "../util/VerticalAlignComponent";
 
-interface LoginComponentProperty{}
+const { useState } = React;
 
-const LoginComponent : React.FunctionComponent<LoginComponentProperty> = (properties: LoginComponentProperty) => {
-    
+interface LoginComponentProperty { }
+
+const LoginComponent: React.FunctionComponent<LoginComponentProperty> = (properties: LoginComponentProperty) => {
+    let [username, setUsername] = useState();
+    let [password, setPassword] = useState();
+    let [errorMessage, setErrorMessage] = useState();
+
+    const onSubmitHandler = (event) => {
+        console.log("This is an error");
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+        event.preventDefault();
+    };
+
+
     return (
         <div className="page component-login">
+            <Modal show={!!errorMessage} hide={() => {setErrorMessage(undefined)}}>
+                <Modal.Title>Error during the login</Modal.Title>
+                <Modal.Body>{errorMessage}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary">Ok</Button>
+                </Modal.Footer>
+            </Modal>
             <VerticalAlignComponent horizontalAlign="center" verticalAlign="center">
-                <Card>
-                    <Card.Body>
-                        This is the login card
-                    </Card.Body>
-                </Card>
+                <Container fluid>
+                    <Row>
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <Card>
+                                <Card.Body>
+                                    <Form onSubmit={onSubmitHandler}>
+                                        <div className="d-grid gap-2">
+                                            <Form.Group>
+                                                <Form.Label>Username</Form.Label>
+                                                <Form.Control onChange={(event: any) => setUsername(event.target.value)}></Form.Control>
+                                            </Form.Group>
+                                            <Form.Group>
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control type="password" onChange={(event: any) => setPassword(event.target.value)}></Form.Control>
+                                            </Form.Group>
+                                            <Button type="submit" variant="primary">Login</Button>
+                                            <Button type="button" variant="secondary">Skip login</Button>
+                                            <Button type="button" variant="link">Registration</Button>
+                                        </div>
+                                    </Form>
+
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </VerticalAlignComponent>
         </div>
     );
 };
 
 export default LoginComponent;
-
