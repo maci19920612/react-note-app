9cdf81a4c4e81d8befca729a935f343752318d88
David Foldesi <david.foldesi@doclerholding.com>
2021-10-11 16:32:42 +0200

    Start the storage manager integration into the AuthManager

--- frontend/src/manager/AuthManager.ts
+++ frontend/src/manager/AuthManager.ts
@@ -1,21 +1,24 @@
-export class AuthManager{
+import { StorageManager } from "./storage/StorageManager";
+
+const KEY_TOKEN = "token"
+
+export class AuthManager {
     constructor(
         private storageManager: StorageManager
-    ){}
-    async login(username: string, password: string) : Promise<void>{
-        if(username != "admin" || password != "admin"){
+    ) { }
+    async login(username: string, password: string): Promise<void> {
+        if (username != "admin" || password != "admin") {
             throw new Error("Invalid credentials provided.");
         }
+        this.storageManager.set(KEY_TOKEN, "debug-token");
     }
-    async loginAsGuest() : Promise<void>{
 
-    }
-    
-    async register(username: string, password: string) : Promise<void>{
+    async register(username: string, password: string): Promise<void> {
 
     }
 
-    async logout(){
-
+    async logout() {
+        this.storageManager.set(KEY_TOKEN, "")
+        //TODO: Logout
     }
 }
